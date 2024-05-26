52651c4972e6922e4b2a268d49d42be896b00c54
Johannes Salminen <johannes.salminen@liaison.com>
2024-05-26 17:26:35 +0300

    add navigation buttons to and from index,results and a button to get results of previous week

--- Pages/Results.razor.cs
+++ Pages/Results.razor.cs
@@ -10,9 +10,15 @@ public partial class Results
     private List<Votes> Votes = [];
     private IEnumerable<ResultRow> ResultSet = [];
     //init result day to last week
-    private DateTime resultDay = VotingService.GetCurrentVotingDay().AddDays(-7);
+    private DateTime resultDay;
     protected override async Task OnInitializedAsync()
     {
+        resultDay = VotingService.GetCurrentVotingDay();
+        await PreviousWeek();
+    }
+    private async Task PreviousWeek() 
+    {
+        resultDay = resultDay.AddDays(-7);
         Votes =  await votingService.GetVotes(resultDay);
         ResultSet = votingService.GetResults(Votes).OrderByDescending(_ => _);
         DisplayNames = await playlistService.GetUserDisplayNames(ResultSet.Select(x => x.AddedBy).ToList());
